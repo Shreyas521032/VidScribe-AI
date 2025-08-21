145a03f654313b5ef46663eea4724ddd527d204c
Shreyas Kasture <149940538+Shreyas521032@users.noreply.github.com>
2025-08-21 22:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def generate_story_with_hf(topic):
     return response.json()[0]['generated_text']
 
 def generate_images_with_hf_api(image_prompts):
-    API_URL = "https://api-inference.huggingface.co/models/playgroundai/playground-v2.5-1024px-aesthetic"
+    API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
     headers = {"Authorization": f"Bearer {st.session_state.hf_token}"}
     image_files = []
     
